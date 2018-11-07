07e4463a791bb5b89f7bf20661301d1791a9681d
Thiago <thiagocasao@gmail.com>
2018-11-07 20:00:34 -0200

    Adicionado relação de pedidos com produtos

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -1,3 +1,9 @@
 class Pedido < ApplicationRecord
   belongs_to :cliente
+
+  has_many :pedido_produtos, inverse_of: :pedido, dependent: :destroy
+  has_many :produtos, through: :pedido_produtos
+  accepts_nested_attributes_for :pedido_produtos
+
+
 end
